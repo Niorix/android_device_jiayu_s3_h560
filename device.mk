e9b4e97b2ec8544aaa1d0b02086cb2b345f3b17a
leskal <alexakis1997@gmail.com>
2017-08-27 01:44:05 +0300

    Add Keymaster HAL interfaces package
    # Conflicts:
    #	product/input.mk

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
 
+# Keymaster HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
 # Camera
 PRODUCT_PACKAGES += \
      Snap \
