2bed107e282c445e39f374295fe1397ade00dcb5
danielhk <daniel_hk>
2017-10-07 05:00:14 +0800

    Add camera HAL implementation for Oero
    # Conflicts:
    #	device.mk

--- device.mk
+++ device.mk
@@ -98,6 +98,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl
 
+# Camera HAL
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
 
 PRODUCT_PACKAGES += \
      Snap \
